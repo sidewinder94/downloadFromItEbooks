07f13c596751743350a2f319a653574ac38dcbed
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2013-06-10 15:16:38 +0200

    Put the try/except block inside the for statement to avoid breaking the entire script when encountering a 404 error

--- downloadFromItEbooks/downloadFromItEbooks.py
+++ downloadFromItEbooks/downloadFromItEbooks.py
@@ -59,59 +59,58 @@ def megaUpload(userName, password, fileName, destination, error=0):
 
 if __name__ == '__main__':
     user = sys.argv[1]
-    password = sys.argv[2]
-    try:
-        for i in range(1,2300):
-           
-            print("http://it-ebooks.info/book/" + str(i))
+    password = sys.argv[2] 
+	for i in range(1,2300):
+		try:
+			print("http://it-ebooks.info/book/" + str(i))
 
-            #On récupère le titre du livre
-            br = Browser()
-            br.open("http://it-ebooks.info/book/" + str(i))
-            file_title = br.title().split(" - ")[0]
+			#On récupère le titre du livre
+			br = Browser()
+			br.open("http://it-ebooks.info/book/" + str(i))
+			file_title = br.title().split(" - ")[0]
 
-            #On récupère le lien de téléchargement
-            response = urllib2.urlopen("http://it-ebooks.info/book/" + str(i))
-            page_source = response.read()
-            myparser = MyParser()
-            myparser.parse(page_source)
+			#On récupère le lien de téléchargement
+			response = urllib2.urlopen("http://it-ebooks.info/book/" + str(i))
+			page_source = response.read()
+			myparser = MyParser()
+			myparser.parse(page_source)
 
-            #On effectue le téléchargement
-            print("Téléchargement du livre : " + file_title)
-            #urllib.urlretrieve ("http://it-ebooks.info" + myparser.getHyperlinks()[0], 
-            #                    file_title + ".pdf")
+			#On effectue le téléchargement
+			print("Téléchargement du livre : " + file_title)
+			#urllib.urlretrieve ("http://it-ebooks.info" + myparser.getHyperlinks()[0], 
+			#                    file_title + ".pdf")
 
-            url = "http://it-ebooks.info" + myparser.getHyperlinks()[0]
+			url = "http://it-ebooks.info" + myparser.getHyperlinks()[0]
 
-            file_name = file_title + ".pdf"
-            u = urllib2.urlopen(url)
-            f = open(file_name, 'wb')
-            meta = u.info()
-            file_size = int(meta.getheaders("Content-Length")[0])
-            print "Downloading: %s Bytes: %s" % (file_name, file_size)
+			file_name = file_title + ".pdf"
+			u = urllib2.urlopen(url)
+			f = open(file_name, 'wb')
+			meta = u.info()
+			file_size = int(meta.getheaders("Content-Length")[0])
+			print "Downloading: %s Bytes: %s" % (file_name, file_size)
 
-            file_size_dl = 0
-            block_sz = 8192
-            while True:
-                buffer = u.read(block_sz)
-                if not buffer:
-                    break
+			file_size_dl = 0
+			block_sz = 8192
+			while True:
+				buffer = u.read(block_sz)
+				if not buffer:
+					break
 
-                file_size_dl += len(buffer)
-                f.write(buffer)
-                status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-                status = status + chr(8)*(len(status)+1)
-                print status,
+				file_size_dl += len(buffer)
+				f.write(buffer)
+				status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+				status = status + chr(8)*(len(status)+1)
+				print status,
 
-            f.close()
+			f.close()
 
-            #Upload chez Mega
-            
-            print("Uploaded : " + file_title + ".pdf" + " Link : " + 
-                  megaUpload(user,password,file_title + ".pdf","Books"))
+			#Upload chez Mega
+			
+			print("Uploaded : " + file_title + ".pdf" + " Link : " + 
+				  megaUpload(user,password,file_title + ".pdf","Books"))
 
-            #On supprime le fichier local du serveur
-            os.remove(file_title + ".pdf")
-            print("Deleted : " + file_title + ".pdf")
-    except Exception, e:
-        print(e)
+			#On supprime le fichier local du serveur
+			os.remove(file_title + ".pdf")
+			print("Deleted : " + file_title + ".pdf")
+		except Exception, e:
+			print(e)
