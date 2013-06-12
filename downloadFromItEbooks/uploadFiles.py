744cafed07e84e24812d0c75245ee4cada1f028f
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2013-06-12 11:35:18 +0200

    Deleted uploaded files and prevented errors

--- downloadFromItEbooks/uploadFiles.py
+++ downloadFromItEbooks/uploadFiles.py
@@ -7,5 +7,9 @@ m = mega.login(sys.argv[1],sys.argv[2])
 directory = m.find("Books")
 for file in os.listdir(sys.argv[3]):
     if (file != sys.argv[0]):
-        mega.upload(arg, directory[0])
-        print arg, "uploaded"
+        try:
+            mega.upload(arg, directory[0])
+            print arg, "uploaded"
+            os.remove(arg)
+        except Exception,e:
+            print e
