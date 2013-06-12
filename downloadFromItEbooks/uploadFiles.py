7baef735228fc90665191bc11f93656e5276f63f
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2013-06-12 10:58:28 +0200

    Added a script allowing to upload all files from a directory

---
+++ downloadFromItEbooks/uploadFiles.py
@@ -0,0 +1,11 @@
+import os
+from mega import Mega
+import sys
+
+mega = Mega()
+m = mega.login(sys.argv[1],sys.argv[2])
+directory = m.find("Books")
+for file in os.listdir(sys.argv[3]):
+    if (file != sys.argv[0]):
+        mega.upload(arg, directory[0])
+        print arg, "uploaded"
